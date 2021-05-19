38a49a83b5b18f2716a8a90f9c41144ed00fcdcf
Carlos Rodriguez <crodriguezq91@gmail.com>
2021-05-18 19:02:55 -0500

    Develop (#3)
    * WIP - finishing hardcoded views
    
    * Updating custom input and sidebar

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -4,7 +4,6 @@ import React from 'react';
 const CustomInput = (props) => {
 
     const { id, disabled, label, className, variant = "outlined" } = props;
-console.log({ id, disabled, label, className, variant })
     return (
         <>
             <TextField disabled={disabled} id={id} label={label} className={className} variant={variant} />
